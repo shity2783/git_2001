00aaac4a53835d3ed0cad67bc13940be3500f991
shity <278388012@qq.com>
2020-08-11 10:33:04 +0800

    这是练习git的项目第一次上传

---
+++ src/test/java/com/baizhi/Git2001ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git2001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("==========");
+    }
+}
